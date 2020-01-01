bef5bb3292c6cd46ac98d673331fba89b789328d
Kjell Magne Fauske <kjellmf@gmail.com>
2020-01-01 15:45:49 +0100

    Create basic vue example skeleton

---
+++ example/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>Hello example</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+export default Vue.extend({
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+});
+</script>
+
+<style>
+</style>
